4762ce9e3bed8fe78a56f4363ea5bec56bcb6a1f
Raaghav <raaghav.4243@gmail.com>
2021-12-20 11:26:19 +0530

    Updates

---
+++ precache-manifest.983126a5fd8d67a0c350480df032e7cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f570f58ba0a2ea5462505acb698827af",
+    "url": "/electro-Vis/index.html"
+  },
+  {
+    "revision": "f949d1823f70671ebb0d",
+    "url": "/electro-Vis/static/css/main.a6008e7a.chunk.css"
+  },
+  {
+    "revision": "6555727d10a339872aa0",
+    "url": "/electro-Vis/static/js/2.aaef5698.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/electro-Vis/static/js/2.aaef5698.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f949d1823f70671ebb0d",
+    "url": "/electro-Vis/static/js/main.62c3187c.chunk.js"
+  },
+  {
+    "revision": "6f83fea7308868d642b5",
+    "url": "/electro-Vis/static/js/runtime-main.20e907c0.js"
+  }
+]);
